65be516950feb9918042d04d5cdcd4e40de9983f
Robby Klein <hello@robbyk.com>
2019-02-05 22:36:58 -0800

    revert sequelize config

--- server/config/sequelize.js
+++ server/config/sequelize.js
@@ -2,11 +2,10 @@ if (!process.env.NODE_ENV) require('dotenv').config()
 
 module.exports = {
     development: {
-        username: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_NAME,
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
+        username: 'root',
+        password: null,
+        database: 'rfunny_dev',
+        host: '127.0.0.1',
         dialect: 'mysql'
     },
     test: {
